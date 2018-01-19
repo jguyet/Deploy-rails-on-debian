e920a3a19fcfd4bcc22cc60d6083c34e4d440e59
Jeremy GUYET <jguyet@e1r10p4.42.fr>
2018-01-19 05:35:14 +0100

    add 2files bash

--- deploy_puma.sh
+++ deploy_puma.sh
@@ -42,7 +42,7 @@ rake db:migration
 rake db:seeds
 rake assets:precompile
 
-export PATH="$PATH:/usr/local/rvm/bin"
+export SECRET_KEY_BASE=`rake secret`
 
 #create shared sockets and logs directory for nginx
 mkdir -p shared/pids shared/sockets shared/log
@@ -105,60 +105,6 @@ server {
 
 #start puma server
 bundle exec puma -C config/puma.rb&
-
-################################################################################
-
-#                                    OR
-
-################################################################################
-#                              UNICORN SERVER
-################################################################################
-echo "gem 'unicorn'" >> Gemfile
-
-bundle install
-
-echo '# set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
-
-
-# Set unicorn options
-worker_processes 2
-preload_app true
-timeout 30
-
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"' > config/unicorn.rb
-
-#add nginx configuration
-echo 'upstream app {
-	server unix:/home/rush-ruby/rush/shared/sockets/unicorn.sock fail_timeout=0;
-}
-
-server {
-	listen 3000 default_server;
-	listen [::]:3000 default_server;
-
-	root /home/rush-ruby/rush/public;
-
-	try_files $uri/index.html $uri @app;
-
-	location @app {
-		proxy_pass http://app;
-	}
-}
-' > /etc/nginx/sites-available/default
-
-#start unicorn server
-bundle exec unicorn -c config/unicorn.rb&
 
 ################################################################################
 
--- deploy_unicorn.sh
+++ deploy_unicorn.sh
@@ -42,73 +42,10 @@ rake db:migration
 rake db:seeds
 rake assets:precompile
 
-export PATH="$PATH:/usr/local/rvm/bin"
+export SECRET_KEY_BASE=`rake secret`
 
 #create shared sockets and logs directory for nginx
 mkdir -p shared/pids shared/sockets shared/log
-
-################################################################################
-#                              PUMA SERVER
-################################################################################
-
-#change rails_env if you want production mode
-echo '
-# Change to match your CPU core count
-workers 2
-
-# Min and Max threads per worker
-threads 1, 6
-
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-
-# Default to production
-rails_env = "development"
-environment rails_env
-
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
-
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
-
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
-' > config/puma.rb
-
-#add nginx configuration
-echo 'upstream app {
-	server unix:/home/rush-ruby/rush/shared/sockets/puma.sock fail_timeout=0;
-}
-
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /home/rush-ruby/rush/public;
-
-	try_files $uri/index.html $uri @app;
-
-	location @app {
-		proxy_pass http://app;
-	}
-}
-' > /etc/nginx/sites-available/default
-
-#start puma server
-bundle exec puma -C config/puma.rb&
-
-################################################################################
-
-#                                    OR
 
 ################################################################################
 #                              UNICORN SERVER
